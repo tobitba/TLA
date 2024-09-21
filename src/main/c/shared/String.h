597dda90a475f7b4229852e09e23e9ca85809b0a
Franco Morroni <fmorroni99@gmail.com>
2024-09-21 04:10:49 -0300

    fixup prev (formatting)

--- src/main/c/shared/String.h
+++ src/main/c/shared/String.h
@@ -13,14 +13,14 @@
  * @param count
  *	The size of the list.
  */
-char * concatenate(const unsigned int count, ...);
+char* concatenate(const unsigned int count, ...);
 
 /**
  * Creates a new string (using heap-memory), that is equal to the original,
  * but using escaped sequences for every control character (i.e., in the range
  * 0x00-0x1F and 0x7F).
  */
-char * escape(const char * string);
+char* escape(const char* string);
 
 /**
  * Generates an indentation string for the specified level, using heap-memory.
@@ -33,6 +33,6 @@ char * escape(const char * string);
  *	The size of every indentation level, thus, level N as a length of N*size
  *	characters.
  */
-char * indentation(const char character, const unsigned int level, const unsigned int size);
+char* indentation(const char character, const unsigned int level, const unsigned int size);
 
 #endif
--- src/main/c/shared/Type.h
+++ src/main/c/shared/Type.h
@@ -1,10 +1,7 @@
 #ifndef TYPE_HEADER
 #define TYPE_HEADER
 
-typedef enum {
-	false = 0,
-	true = 1
-} boolean;
+typedef enum { false = 0, true = 1 } boolean;
 
 typedef int Token;
 
